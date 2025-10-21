9d4e756dfbf967b705736a4cc1a23c99078b4548
vladpro42 <bondarev.vlad.01@gmail.com>
2025-10-21 13:33:52 +0300

    feat: resolve cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+
 const parseArgs = () => {
-    // Write your code here 
+    let result = '';
+
+    const argv = process.argv.slice(2)
+    for (let index = 0; index < argv.length; index += 2) {
+        const propName = argv[index].replace('--', '')
+        const value = argv[index + 1]
+        if (result) result += ', '
+        result += `${propName} is ${value}`
+    }
+    console.log(result)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env
+    // envs['RSS_var1'] = 'val1'
+    // envs['RSS_var2'] = 'val2'
+    const PREFIX = 'RSS_'
+    let str = '';
+
+    for (const [key, value] of Object.entries(envs)) {
+        if (key.startsWith(PREFIX)) {
+            str += key.toString() + '=' + value.toString() + '; '
+        }
+    }
+
+    console.log(str)
 };
 
 parseEnv();
