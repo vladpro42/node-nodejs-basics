006638b2080f13f8cd26587c6ab932bb8f0aaff5
vladpro42 <bondarev.vlad.01@gmail.com>
2025-10-21 14:07:19 +0300

    feat: resolve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from "node:crypto"
+import path from "node:path";
+import { createReadStream } from "node:fs";
+import { pipeline } from "stream/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = path.resolve('src', 'hash', 'files', 'fileToCalculateHashFor.txt')
+    try {
+        const readStream = createReadStream(fileName)
+        const hash = createHash('sha256')
+        await pipeline(readStream, hash)
+        const hexHash = hash.digest('hex')
+        console.log(hexHash)
+    } catch (error) {
+        console.error('Error calculating hash or something else');
+    }
 };
 
 await calculateHash();
