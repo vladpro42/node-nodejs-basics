cc7849305520a939b196c565aefb097e10a3f96c
vladpro42 <bondarev.vlad.01@gmail.com>
2025-10-21 14:30:22 +0300

    feat: resolve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import path from "path"
+import zlib from "zlib"
+import { pipeline } from "stream/promises";
+import { createWriteStream, createReadStream } from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const filename = path.resolve('src', 'zip', 'files', 'fileToCompress.txt')
+        const source = createReadStream(filename)
+        const gzip = zlib.createGzip();
+        const dest = createWriteStream(path.resolve('src', 'zip', 'files', 'archive.gz'))
+        await pipeline(source, gzip, dest)
+    }
+    catch (error) {
+        console.log(error)
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import path from "path"
+import zlib from "zlib"
+import { pipeline } from "stream/promises";
+import { createWriteStream, createReadStream } from "fs";
+
+
 const decompress = async () => {
-    // Write your code here 
+    const filename = path.resolve('src', 'zip', 'files', 'archive.gz')
+    const source = createReadStream(filename)
+    const guzip = zlib.createGunzip();
+    const dest = createWriteStream(path.resolve('src', 'zip', 'files', 'fileToCompress.txt'))
+    await pipeline(source, guzip, dest)
 };
 
 await decompress();
