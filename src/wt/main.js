f260be133dccdc5b56b43212a2828ab552edfdf9
vladpro42 <bondarev.vlad.01@gmail.com>
2025-10-26 19:15:31 +0300

    feat: resolve wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,56 @@
+import os from "os"
+import { resolve } from "path";
+import { Worker } from "worker_threads";
+import path from "path"
+import fs from "fs/promises"
+
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCorese = os.cpus().length;
+    console.log(`amount of cpu cores equal: ${cpuCorese}`)
+
+    const workerPromises = []
+    const workers = []
+
+    for (let index = 0; index < cpuCorese; index++) {
+        const numberToSend = 10 + index;
+        const workerPromise = new Promise((res, rej) => {
+            const worker = new Worker(path.resolve('src', 'wt', 'worker.js'))
+            workers.push(worker)
+            worker.on('message', (result) => {
+                if (result.success === 'success') {
+                    res({ status: 'resolved', data: result.data, index, })
+                } else {
+                    resolve({ status: 'error', data: null, index, })
+                }
+                worker.terminate();
+            })
+            worker.on('error', err => {
+                console.error(`error in worker ${index}: `, err)
+                res({
+                    status: 'error',
+                    data: null,
+                    index,
+                })
+                worker.terminate()
+            })
+            worker.on('exit', code => {
+                if (code !== 0) {
+                    console.log(`Worker ${index} finished with code ${code}`)
+                }
+            })
+            worker.postMessage(numberToSend)
+        })
+        workerPromises.push(workerPromise)
+    }
+    try {
+        const results = await Promise.all(workerPromises)
+        console.log('workers results')
+        console.log(results)
+    } catch (error) {
+        console.error('error with completed workers: ', error)
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,25 @@
-// n should be received from main thread
+import { parentPort } from "worker_threads";
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (result) => {
+    parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.on('message', (data) => {
+    try {
+        console.log(`Worker received data: ${data}`);
+        const result = nthFibonacci(data);
+
+        sendResult({
+            success: 'success',
+            data: result
+        });
+    } catch (err) {
+        sendResult({
+            success: 'error',
+            data: null,
+            error: err.message
+        });
+    }
+});
